bb3584c063aff2cfe4f723fbb63c06e99ba5e731
baixiaoqian <baixiaoqian@danale.com>
2019-06-12 18:00:35 +0800

    docs(medium): code save
    保存代码

---
+++ medium/ch16/main.go
@@ -0,0 +1,13 @@
+package main
+
+//最接近的三个数之和
+func threeSumClosest(nums []int, target int) int {
+	var res int
+	return res
+}
+
+func main() {
+	nums := []int{}
+	res := threeSumClosest(nums, 9)
+	println(res)
+}
